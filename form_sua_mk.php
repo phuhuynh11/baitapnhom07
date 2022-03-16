30ae7c4052304a953f6259a96ef1029b4be9875c
phuhuynh11 <68230175+phuhuynh11@users.noreply.github.com>
2022-03-16 09:17:19 +0700

    Add files via upload

---
+++ form_sua_mk.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="luu.php" method="post">
+Password cu: <input type="password" name="password"><br>
+Password moi: <input type="password" name="pass"><br>
+Xac Nhan Password moi: <input type="password" name="pass"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
